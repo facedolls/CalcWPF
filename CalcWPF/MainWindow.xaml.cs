df1c87d181aacab84e16771bde843d647b3aa854
Dennis T <facedolls@gmail.com>
2020-12-14 18:12:28 +0200

    Generalize operations.

--- CalcWPF/MainWindow.xaml.cs
+++ CalcWPF/MainWindow.xaml.cs
@@ -20,8 +20,8 @@ namespace CalcWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        int num1 = 0;
-        int num2 = 0;
+        double num1 = 0;
+        double num2 = 0;
         string operation = "";
         public MainWindow()
         {
@@ -31,27 +31,37 @@ namespace CalcWPF
         private void btn_num_Click(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
-            String str = button.Content.ToString();
-            int num = Int32.Parse(str);
+            String num = button.Content.ToString();
+            if (textBox.Text == "0")
+                textBox.Text = num;
+            else
+                textBox.Text += num;
 
-            if(operation == "")
+            if (operation == "")
             {
-                num1 = num1 * 10 + num;
-                textBox.Text = num1.ToString();
+                num1 = double.Parse(textBox.Text);
             }
             else
             {
-                num2 = num2 * 10 + num;
-                textBox.Text = num2.ToString();
+                num2 = double.Parse(textBox.Text);
             }
+        }
+        private void btn_op_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            operation = button.Content.ToString();
         }
         private void btn_eq_Click(object sender, RoutedEventArgs e)
         {
-            int result = 0;
+            double result = 0;
             switch (operation)
             {
+                case "x^y":
+                    result = Math.Pow(num1,num2);
+                    break;
                 case "/":
-                    result = num1 / num2;
+                    if(num2 > 0)
+                        result = num1 / num2;
                     break;
                 case "*":
                     result = num1 * num2;
@@ -62,27 +72,93 @@ namespace CalcWPF
                 case "+":
                     result = num1 + num2;
                     break;
+                case "min":
+                    result = Math.Min(num1, num2);
+                    break;
+                case "max":
+                    result = Math.Max(num1, num2);
+                    break;
+                case "avg":
+                    result = (num1 + num2) / 2;
+                    break;
             }
             textBox.Text = result.ToString();
             operation = "";
             num1 = result;
+            num2 = 0;
         }
 
-        private void btn_div_Click(object sender, RoutedEventArgs e)
+        private void btn_c_Click(object sender, RoutedEventArgs e)
         {
-            operation = "/";
+            num1 = 0;
+            num2 = 0;
+            operation = "";
+            textBox.Text = "0";
         }
-        private void btn_mult_Click(object sender, RoutedEventArgs e)
+
+        private void btn_ce_Click(object sender, RoutedEventArgs e)
         {
-            operation = "*";
+            if (operation == "")
+                num1 = 0;
+            else
+                num2 = 0;
+            textBox.Text = "0";
         }
-        private void btn_minus_Click(object sender, RoutedEventArgs e)
+        private void btn_backspace_Click(object sender, RoutedEventArgs e)
         {
-            operation = "-";
+            textBox.Text = DropLastChar(textBox.Text);
+            if (operation == "")
+                num1 = double.Parse(textBox.Text);
+            else
+                num2 = double.Parse(textBox.Text);
         }
-        private void btn_plus_Click(object sender, RoutedEventArgs e)
+
+        private string DropLastChar(string text)
         {
-            operation = "+";
+            if (text.Length == 1)
+                text = "0";
+            else
+            {
+                text = text.Remove(text.Length - 1, 1);
+                if(text[text.Length - 1] == '.' || text[text.Length - 1] == ',')
+                    text = text.Remove(text.Length - 1, 1);
+            }
+            return text;
+        }
+
+        private void btn_plusminus_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                num1 *= -1;
+                textBox.Text = num1.ToString();
+            }
+            else
+            {
+                num2 *= -1;
+                textBox.Text = num2.ToString();
+            }
+        }
+
+        private void btn_comma_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+                SetComma(num1);
+            else
+                SetComma(num2);
+        }
+
+        private void SetComma(double num)
+        {
+            if (textBox.Text.Contains('.') || textBox.Text.Contains(','))
+                return;
+            else
+                textBox.Text += ',';
+        }
+
+        private void btn_7_KeyUp(object sender, KeyEventArgs e)
+        {
+
         }
     }
 }
